24ac41da1c56d56253bba84b3fc3e3c47dd67db0
irishmanae1 <irishmanae123@icloud.com>
2025-05-29 16:09:08 -0400

    Initial Commit

---
+++ CryptoCheck/CryptoCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCheckApp.swift
+//  CryptoCheck
+//
+//  Created by Liam Mulligan on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
