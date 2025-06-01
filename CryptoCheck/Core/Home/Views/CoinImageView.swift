cca9aa12663fc858db49723b08c8e5d9209b97c9
irishmanae1 <irishmanae123@icloud.com>
2025-06-01 13:58:04 -0400

    [Feature] Built CoinImageService (uses Coin to download coresponding image), CoinImageViewModel, and CoinImageView

---
+++ CryptoCheck/Core/Home/Views/CoinImageView.swift
@@ -0,0 +1,38 @@
+//
+//  CoinImageView.swift
+//  CryptoCheck
+//
+//  Created by Liam Mulligan on 6/1/25.
+//
+
+import SwiftUI
+
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: Coin) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    CoinImageView(coin: DeveloperPreview.instance.coin)
+        .padding()
+}
--- CryptoCheck/Core/Home/Views/CoinImageViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CoinImageViewModel.swift
-//  CryptoCheck
-//
-//  Created by Liam Mulligan on 6/1/25.
-//
-
-import Foundation
