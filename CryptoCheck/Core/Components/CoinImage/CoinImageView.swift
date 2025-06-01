9d4342efbe311149a25c3f1768c8950aa05c3772
irishmanae1 <irishmanae123@icloud.com>
2025-06-01 12:05:44 -0400

    [Feature] Created CoinImageView and CoinImageViewModel

---
+++ CryptoCheck/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinImageView.swift
+//  CryptoCheck
+//
+//  Created by Liam Mulligan on 6/1/25.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinImageView()
+}
---
+++ CryptoCheck/Core/Home/Views/CoinImageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoinImageViewModel.swift
+//  CryptoCheck
+//
+//  Created by Liam Mulligan on 6/1/25.
+//
+
+import Foundation
