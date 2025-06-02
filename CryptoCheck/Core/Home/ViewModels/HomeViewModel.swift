687b0a198c45f50de58f3ba86ab35d0826a1dcc9
irishmanae1 <irishmanae123@icloud.com>
2025-06-01 20:47:22 -0400

    [Feature] Updated HomeViewModel to sort coins with input from SearchBarView

--- CryptoCheck/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoCheck/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,11 +23,30 @@ class HomeViewModel: ObservableObject {
     
     func addSubscriber() {
         
-        dataService.$allCoins
-            .sink { (returnedCoins) in
-                self.allCoins = returnedCoins
+        // updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
     }
+    
+    private func filterCoins(text: String, coins: [Coin]) -> [Coin] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.contains(lowercasedText) ||
+            coin.id.contains(lowercasedText)
+        }
+    }
+        
     
 }
